7571e78c5755cbd5e967cb08a56f3482455a9155
Pedro Felipe Gómez <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-09-30 15:37:14 -0500

    Creacion clase Producto / Creacion de constructores

---
+++ Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoAula
+{
+    class Producto
+    {
+        public String nombre;
+        public double isbn;
+        public String tipo;
+        public String marca;
+        public String proveedor;
+        public double precio;
+
+        public Producto() { }
+        public Producto(String nombre,double isbn,String tipo,String marca,String proveedor,double precio)
+        {
+            this.nombre = nombre;
+            this.isbn = isbn;
+            this.tipo = tipo;
+            this.marca = marca;
+            this.proveedor = proveedor;
+            this.precio = precio;
+        }
+    }
+}
