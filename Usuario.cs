258d4ef2fe9a5a86b876ce1916101655ea3e0074
Pedro Felipe Gómez <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-10-07 15:33:25 -0500

    Creacion de clase Usuario con getters y setters, ademas de un nuevo DatePicker

---
+++ Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoAula
+{
+    class Usuario
+    {
+        private String nombre;
+        private String apellido;
+        private String sexo;
+        private String direccion;
+        private String ciudad;
+        private String fechaNacimiento;
+        private int edad;
+        private String departamento;
+        private String nacionalidad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Ciudad { get => ciudad; set => ciudad = value; }
+        public string FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Departamento { get => departamento; set => departamento = value; }
+        public string Nacionalidad { get => nacionalidad; set => nacionalidad = value; }
+
+        public Usuario() { }
+
+    }
+}
